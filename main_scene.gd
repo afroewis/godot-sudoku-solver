212d2df581e495be6e93a43d5c36a0ed61609ab5
Andreas Fröwis <andreas.froewis@gmail.com>
2021-05-20 23:15:11 +0200

    Renamed stuff

--- main_scene.gd
+++ main_scene.gd
@@ -1,6 +1,6 @@
 extends Control
 
-export(PackedScene) var rect_scene = preload("res://color_rect.tscn")
+export(PackedScene) var rect_scene = preload("res://number.tscn")
 
 onready var button = get_node("Button")
 onready var container = get_node("GridContainer")
@@ -16,12 +16,24 @@ func on_pressed():
 	container.add_child(instance)
 	
 func set_up():
-	var board = [3, 2, 1]
+	var board = [
+		0, 0, 0, 0, 1, 0, 0, 0, 2,
+		0, 0, 0, 0, 0, 3, 0, 1, 0,
+		0, 0, 4, 0, 0, 5, 0, 0, 6,
+		0, 0, 0, 6, 0, 0, 7, 5, 0,
+		0, 0, 1, 0, 0, 0, 8, 0, 0,
+		0, 8, 3, 0, 0, 9, 0, 0, 0,
+		8, 0, 0, 7, 0, 0, 9, 0, 0,
+		0, 2, 0, 5, 0, 0, 0, 0, 0,
+		7, 0, 0, 0, 4, 0, 0, 0, 0
+	]
 	
 	for x in board:
-		var instance = rect_scene.instance()
-		var label = instance.get_node("Label")
-		label.text = String(x)
+		var instance = rect_scene.instance();
+		var label = instance.get_node("Label");
+		
+		if [x == 0]:
+			label.text = String(x)
 		
 		container.add_child(instance)
 
